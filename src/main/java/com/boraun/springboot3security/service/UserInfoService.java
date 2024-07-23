8c22ad22efc2dd8c481222b8701b1f52cb392a41
Bora Un <74696117+UnBora@users.noreply.github.com>
2024-07-23 22:51:11 +0700

    *springboot3security

---
+++ src/main/java/com/boraun/springboot3security/service/UserInfoService.java
@@ -0,0 +1,41 @@
+package com.boraun.springboot3security.service;
+
+import com.boraun.springboot3security.entity.UserInfo;
+import com.boraun.springboot3security.repository.UserInfoRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Data
+public class UserInfoService implements UserDetailsService {
+
+
+    private UserInfoRepository repository;
+
+
+    private PasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<UserInfo> userDetail = repository.findByName(username);
+
+        // Converting userDetail to UserDetails
+        return userDetail.map(UserInfoDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found " + username));
+    }
+
+    public String addUser(UserInfo userInfo) {
+        userInfo.setPassword(encoder.encode(userInfo.getPassword()));
+        repository.save(userInfo);
+        return "User Added Successfully";
+    }
+}
+
